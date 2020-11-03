274508153458204dd3209e9b7f7c51df6f04b62f
joshua oxner <bohemus@hotmail.com>
2020-11-03 09:28:30 -0700

    nd adjusting cache as needed

--- client/src/requests.js
+++ client/src/requests.js
@@ -1,13 +1,40 @@
 import {getAccessToken, isLoggedIn} from './auth';
 import gql from 'graphql-tag';
-import { ApolloClient, HttpLink, InMemoryCache } from 'apollo-boost';
+import { ApolloClient, ApolloLink, HttpLink, InMemoryCache } from 'apollo-boost';
 
-const endpointUrl = 'http://localhost:9000/graphql'
+const endpointUrl = 'http://localhost:9000/graphql';
+
+const authLink = new ApolloLink((operation, forward) => {
+  if (isLoggedIn()) {
+        operation.setContext({
+          headers: {
+            'authorization': 'Bearer ' + getAccessToken()
+          }
+        })
+      }
+  return forward(operation);
+})
 
 const client = new ApolloClient({
-  link: new HttpLink({uri: endpointUrl}),
+  link: ApolloLink.from([
+    authLink,
+    new HttpLink({uri: endpointUrl}),
+  ]), 
   cache: new InMemoryCache(),
 })
+
+const jobQuery = gql`query JobQuery($id: ID!) {
+  job(id: $id ) {
+    id
+    title
+    company {
+      id 
+      name
+    }
+    description
+  }
+}
+`;
 
 // const graphQlRequest = async (query, variables = {}) => {
 //   const request = {
@@ -38,23 +65,12 @@ export const loadJobs = async () => {
         }
       }
     }`;
-  const {data: {jobs}} = await client.query({query})
+  const {data: {jobs}} = await client.query({query, cache: 'no-cache'})
   return jobs;
 }
 
 export const loadJob = async (id) => {
-  const query = gql`query JobQuery($id: ID!) {
-      job(id: $id ) {
-        id
-        title
-        company {
-          id 
-          name
-        }
-        description
-      }
-    }`
-  const {data: {job}} = await client.query({query, variables: {id}})
+  const {data: {job}} = await client.query({query: jobQuery, variables: {id}})
   return job;
 }
 
@@ -84,9 +100,19 @@ export const createJob = async(input) => {
           id 
           name
         }
+        description
       }
     }`;
-  const {data: {job}} = await client.mutate({mutation, variables: {input} })
+  const {data: {job}} = await client.mutate({
+    mutation,
+    variables: {input},
+    update: (cache, {data}) => {
+      cache.writeQuery({
+        query: jobQuery,
+        variables: {id: data.job.id},
+        data
+      })
+    }})
   return job;
 }
 
